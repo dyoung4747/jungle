b8387d9dbdf730da2a12aa7ccf02ed6bff2ddfb0
Douglas Young <dyoung4747@hotmail.com>
2022-01-20 21:47:37 +0000

    implement sales path and functionality

---
+++ app/controllers/admin/sales_controller.rb
@@ -0,0 +1,30 @@
+class Admin::SalesController < ApplicationController
+
+  def index
+    @sales = Sale.all
+  end
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  def sale_params
+    params.require(:sale).permit(
+      :name,
+      :starts_on,
+      :ends_on,
+      :percent_off
+    )
+  end
+
+end
